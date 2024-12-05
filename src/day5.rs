975ea0bd8077051397667d5f02d6322d6ddf81fa
David García Morillo <davidgmorillop@gmail.com>
2024-12-05 15:16:58 +0100

    Optimise Day 5 after ashamedly seeing Reddit

--- src/day5.rs
+++ src/day5.rs
@@ -1,26 +1,11 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 type Rules = HashMap<u8, Vec<u8>>;
 type Update = Vec<u8>;
 
-fn is_ordered(update: &Update, rules: &Rules) -> bool {
-    update.iter().enumerate().all(|(i, n)| {
-        if let Some(numbers_to_check) = rules.get(n) {
-            numbers_to_check.iter().all(|number| {
-                update
-                    .iter()
-                    .position(|past_number| past_number == number)
-                    .map_or(true, |j| j < i)
-            })
-        } else {
-            true
-        }
-    })
-}
-
 fn part1(updates: &[Update], rules: &Rules) {
     let result = updates.iter().fold(0, |acc, update| {
-        let is_ordered = is_ordered(update, rules);
+        let is_ordered = update.is_sorted_by(|a, b| rules.get(a).unwrap().contains(b));
         if is_ordered {
             let length = update.len();
             acc + update[(length - 1) / 2] as usize
@@ -34,33 +19,18 @@ fn part1(updates: &[Update], rules: &Rules) {
 
 fn part2(updates: &[Update], rules: &Rules) {
     let result = updates.iter().fold(0, |acc, update| {
-        let is_ordered_ = is_ordered(update, rules);
+        let ordered = update.is_sorted_by(|a, b| rules.get(a).unwrap().contains(b));
         let length = update.len();
 
-        if !is_ordered_ {
+        if !ordered {
             let mut new_update = update.clone();
-            let mut curr_index = 0;
-            while curr_index < update.len() {
-                if let Some(numbers_to_check) = rules.get(&new_update[curr_index]) {
-                    let furthest_index = numbers_to_check
-                        .iter()
-                        .filter_map(|number| {
-                            new_update
-                                .iter()
-                                .position(|past_number| past_number == number)
-                        })
-                        .max();
-
-                    if let Some(position) = furthest_index {
-                        if curr_index < position {
-                            // https://stackoverflow.com/a/62152815
-                            new_update[curr_index..=position].rotate_left(1);
-                            continue;
-                        }
-                    }
+            new_update.sort_by(|a, b| {
+                if rules.get(a).unwrap().contains(b) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
                 }
-                curr_index += 1;
-            }
+            });
             acc + new_update[(length - 1) / 2] as usize
         } else {
             acc
@@ -79,9 +49,9 @@ pub fn day5() {
     rules_string.lines().for_each(|l| {
         let (l, r) = l.split_once('|').unwrap();
         rules
-            .entry(r.parse().unwrap())
+            .entry(l.parse().unwrap())
             .or_default()
-            .push(l.parse().unwrap());
+            .push(r.parse().unwrap());
     });
 
     let updates: Vec<Update> = updates_string
